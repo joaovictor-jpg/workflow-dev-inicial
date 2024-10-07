82f0c50e333e3a28d1c7324f1c05e64a9e47e262
João Victor <joaovictormdasilva676@gmail.com>
2024-10-07 15:29:40 -0300

    feat::sparkles:add: métodos para listar livros por autor

--- src/models/autor.js
+++ src/models/autor.js
@@ -63,6 +63,10 @@ class Autor {
     const resultado = await this.criar();
     return resultado;
   }
+
+  static async pegarLivroPorAutor(autorId) {
+    return db('livros').where({ autor_id: autorId });
+  }
 }
 
 export default Autor;
